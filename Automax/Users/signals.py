9c42a352354a4308a47b6cd71e48e8e1e06bc5ec
NellyMakena <nellysolomonmakena3@gmail.com>
2024-09-26 16:59:26 +0300

    Automax

---
+++ Automax/Users/signals.py
@@ -0,0 +1,19 @@
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
+from .models import Profile,Location
+
+
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)  # 'user' should match the field name in Profile model
+
+
+@receiver(post_save, sender=Profile)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        profile_location = Location.objects.create()
+        instance.location= profile_location
+        instance.save()
